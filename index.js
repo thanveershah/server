10f27d0eb3fec0479f115198e1ab4100bbd67e15
thanveershah <thanveershah92@gmail.com>
2023-09-16 20:19:17 +0400

    updated text

--- index.js
+++ index.js
@@ -10,15 +10,19 @@ require("dotenv").config();
 connectDb();
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+   cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", userRoute);
+
+app.use("*", (req, res) => {
+   res.json("This is a 404 page");
+});
 
 const port = 3003;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
