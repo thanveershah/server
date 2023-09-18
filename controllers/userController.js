db70eeb6fb38ef10530ab0b8a56d7e90200671f3
thanveershah <thanveershah92@gmail.com>
2023-09-18 23:47:27 +0400

    some code

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,6 +36,7 @@ const login = async (req, res) => {
       httpOnly: false,
       secure: true,
       sameSite: "none",
+      domain: "a5a0-2001-8f8-1473-3303-a880-a31a-1023-3bdc.ngrok-free.app",
       path: "/",
    });
 
