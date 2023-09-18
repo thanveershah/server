6ec7038d63cccbd80442a988e9ca0ea9b2fda8bb
thanveershah <thanveershah92@gmail.com>
2023-09-18 23:44:52 +0400

    remove http

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,9 +33,10 @@ const login = async (req, res) => {
    const refreshToken = "testing_refresh";
 
    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
+      httpOnly: false,
       secure: true,
       sameSite: "none",
+      domain: "https://a5a0-2001-8f8-1473-3303-a880-a31a-1023-3bdc.ngrok-free.app",
       path: "/",
    });
 
